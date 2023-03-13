4a4e1ecaf75c9081be7f5f0927c27c2612854435
70by666 <70bytriplesix666@gmail.com>
2023-03-14 01:11:11 +0300

    BlogPostsAdmin

--- apps/blog/admin.py
+++ apps/blog/admin.py
@@ -7,8 +7,8 @@ from apps.blog.models import BlogPosts
 class BlogPostsAdmin(admin.ModelAdmin):
     list_display = ('title', 'id', 'status', 'created', 'author')
     fields = (
-        ('id', 'title', 'status', 'slug'), 
-        ('created', 'updated'), 'author', 'updater'
-        'image', ('short_descrtiprion', 'full_description'), 
+        ('id', 'title', 'slug'), 
+        ('author', 'created'), ('updater', 'updated'),
+        ('status', 'image', 'short_descrtiprion'), 'full_description', 
     )
-    readonly_fields = ('id', 'created')
+    readonly_fields = ('id', 'created', 'updated')
