a31208c6e17fb4aee908ecea45c41db1a70386c3
70by666 <70bytriplesix666@gmail.com>
2023-03-14 11:13:17 +0300

    CommonContextMixin

---
+++ common/views.py
@@ -0,0 +1,8 @@
+class CommonContextMixin:
+    title = None
+    
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = self.title
+        
+        return context
