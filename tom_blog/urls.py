095ef22a90d3c4351287bf6ea8fa3a0e9d48152f
70by666 <70bytriplesix666@gmail.com>
2023-03-14 11:17:23 +0300

    IndexView, ContactView, blog app

--- tom_blog/urls.py
+++ tom_blog/urls.py
@@ -18,9 +18,14 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 
+from tom_blog.views import IndexView, ContactView
+
     
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', IndexView.as_view(), name='index'),
+    path('contact/', ContactView.as_view(), name='contact'),
+    path('blog/', include('apps.blog.urls', namespace='blog')),
 ]
 
 if settings.DEBUG:
