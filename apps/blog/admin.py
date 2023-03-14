691f6e178d9c820ec32380e0a7f3c5c17119330a
70by666 <70bytriplesix666@gmail.com>
2023-03-14 11:07:27 +0300

    CategoriesAmin and small fixes

--- apps/blog/admin.py
+++ apps/blog/admin.py
@@ -1,14 +1,36 @@
 from django.contrib import admin
 
-from apps.blog.models import BlogPosts
+from mptt.admin import DraggableMPTTAdmin
+
+from apps.blog.models import Posts, Categories
 
 
-@admin.register(BlogPosts)
+@admin.register(Posts)
 class BlogPostsAdmin(admin.ModelAdmin):
+    """
+    Отображение модели блога в админке
+    """
+    
     list_display = ('title', 'id', 'status', 'created', 'author')
     fields = (
-        ('id', 'title', 'slug'), 
+        ('id', 'title', 'slug', 'category'), 
         ('author', 'created'), ('updater', 'updated'),
         ('status', 'image', 'short_descrtiprion'), 'full_description', 
     )
+    prepopulated_fields = {'slug': ('title',)}
     readonly_fields = ('id', 'created', 'updated')
+
+
+@admin.register(Categories)
+class CategoriesAmin(DraggableMPTTAdmin):
+    """
+    Отображение модели категорий в админке
+    """
+    
+    list_display = ('tree_actions', 'indented_title', 'id', 'title', 'slug')
+    list_display_links = ('title', 'slug')
+    prepopulated_fields = {'slug': ('title',)}
+    fieldsets = (
+        ('Основная информация', {'fields': ('title', 'slug', 'parent')}),
+        ('Описание', {'fields': ('description',)})
+    )
