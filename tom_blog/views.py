c1571bb200d7d239fe46fd1637181961820789fc
70by666 <70bytriplesix666@gmail.com>
2023-03-14 15:25:27 +0300

    ListView

--- tom_blog/views.py
+++ tom_blog/views.py
@@ -1,13 +1,22 @@
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView, ListView
 
-from common.views import CommonContextMixin
+from common.views import TitleMixin
+from apps.blog.models import Posts
 
 
-class IndexView(CommonContextMixin, TemplateView):
+class IndexView(TitleMixin, ListView):
+    model = Posts
     template_name = 'index.html'
     title = 'Домашняя страница' 
 
-
-class ContactView(CommonContextMixin, TemplateView):
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        queryset = super().get_queryset()
+        context['filter'] = queryset.order_by('created')[:3]
+        
+        return context
+    
+    
+class ContactView(TitleMixin, TemplateView):
     template_name = 'contact.html'
     title = 'Обратная связь'
