e18b5e613ca9b013fa4f86519ae4ca472d24b8ed
70by666 <70bytriplesix666@gmail.com>
2023-04-10 11:07:26 +0300

    comments fixes

--- apps/blog/views.py
+++ apps/blog/views.py
@@ -158,8 +158,10 @@ class CommentCreateView(SuccessMessageMixin, IpMixin, LoginRequiredMixin, Create
         post = Posts.objects.get(slug=self.kwargs['slug'])
         form.instance.post = post
         form.instance.author = self.request.user
+        print(Comments.objects.filter(parent=self.kwargs['parent']))
         if not self.kwargs['parent'] == int(0):
-            parent = Comments.objects.get(parent=self.kwargs['parent'])
+            print(self.kwargs['parent'])
+            parent = Comments.objects.get(id=self.kwargs['parent'])
             form.instance.parent = parent
     
         return super().form_valid(form)
